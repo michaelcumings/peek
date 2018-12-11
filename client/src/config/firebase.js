9e0446977d57233091c332808a1b1c90dc435776
sadiyaj <sadiyajiru@gmail.com>
2018-12-10 20:15:50 -0600

    I modified login.js app.js privatePage.js and config folder which has the firebase account setting

---
+++ client/src/config/firebase.js
@@ -0,0 +1,16 @@
+/////Created by Sadiya 
+//// This is our firebase settings
+
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCTtdBveH4rex039u5Fh_gEXNbK37ko1zQ",
+    authDomain: "peek-2fb5b.firebaseapp.com",
+    databaseURL: "https://peek-2fb5b.firebaseio.com",
+    projectId: "peek-2fb5b",
+    storageBucket: "peek-2fb5b.appspot.com",
+    messagingSenderId: "511969212938"
+  };
+
+  const fb = firebase.initializeApp(config);
+export default fb;
