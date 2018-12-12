9f686309e7e7783889bd48d004a0849cf3437660
Nick Sedillos <nick.sedillos@gmail.com>
2018-12-11 19:24:33 -0600

    Added Title component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 ///Created by Sadiya 
-///Point of entery for the application
+// Point of entry for the application.
 
 import React, { Component } from 'react';
 // import './App.css';
+import Title from './components/Title';
 import fb from './config/firebase';
 import PrivatePage from './pages/PrivatePage';
 import Login from './pages/Login'
 class App extends Component {
-  //constructor for putting the user in a state
+  // Constructor for putting the user in a state.
   constructor(props) {
     super(props);
 
@@ -16,11 +17,11 @@ class App extends Component {
     });
     this.authListener = this.authListener.bind(this);
   }
-  //After the component is done rendering for the first time 
+  // After the component is done rendering for the first time.
   componentDidMount() {
     this.authListener();
   }
-//called whenever the authentication changes 
+// Called whenever the authentication changes.
   authListener() {
     fb.auth().onAuthStateChanged((user) => {
       console.log(user);
@@ -34,10 +35,9 @@ class App extends Component {
     });
   }
   render() {
-    //Check if the user is logged in. 
-    //if this.state.user is true render Home otherwise take them to login page
     return (
      <div className = "App">
+     <Title></Title>
      {this.state.user ? ( <PrivatePage/>) : (<Login />)}
      </div>
     );
