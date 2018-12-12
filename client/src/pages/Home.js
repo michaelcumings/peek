f17505c8f2fec2cc9d138f854504cd3e28eeca51
Nick Sedillos <nick.sedillos@gmail.com>
2018-12-11 20:01:13 -0600

    Added Home.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+				<div className="jumbotron jumbotron-fluid">
+					<div class="container">
+						<h1 class="display-4">Fluid jumbotron</h1>
+						<p class="lead">Placeholder for the map view?</p>
+					</div>
+				</div>
+				<button type="button" class="btn btn-primary">New Peek</button>
+			</div>
+    );
+  }
+}
+
+export default Home;
