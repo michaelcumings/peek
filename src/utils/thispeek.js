5b995cd5b243523d60affad61375f6ef176a0763
Michael Cumings <michael.cumings@gmail.com>
2018-12-15 12:02:48 -0600

    start of error checking

--- src/utils/thispeek.js
+++ src/utils/thispeek.js
@@ -3,19 +3,45 @@ import firebase from "firebase";
 export default {
     write: (newpeek) => {
 
+        if (!newpeek.image) {
+            return "Error-No Image";
+        }
+
+        if (!newpeek.location) {
+            return "Error-No Location";
+        }
+
+        let vends = false;
+        if (newpeek.ends = "on") {
+            vends = true;
+        }
+
+        let vprivate = false;
+        if (newpeek.private = "on") {
+            vprivate = true;
+        }
+
+        var user = firebase.auth().currentUser;
+
+        const vusername = (user.displayName || user.email);
+        const vtag = (newpeek.tag || "");
+        const vdescription = (newpeek.description || "No Description");
+        const vimage = newpeek.image;
+        const vlocation = newpeek.location; 
+
         const db = firebase.firestore();
         db.settings({
         timestampsInSnapshots: true
         });
         db.collection("peeks").add({
-        username: newpeek.username,
-        tag: newpeek.tag,
-        userID: firebase.auth().currentUser.uid,
-        description: newpeek.description,
-        image: newpeek.image,
-        location: newpeek.location,
-        ends: newpeek.ends,
-        private: newpeek.private
+        username: vusername,
+        tag: vtag,
+        userID: user.uid,
+        description: vdescription,
+        image: vimage,
+        location: vlocation,
+        ends: vends,
+        private: vprivate
         });  
     },
 };
