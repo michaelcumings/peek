ba5c7c34c22b1a94ce084121d1704afbd3079101
Nick Sedillos <nick.sedillos@gmail.com>
2018-12-17 12:24:45 -0600

    Updated ESLint config with React settings and revised Nav component.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
-function Nav() {
-  return (
-		<ul className="nav nav-tabs">
-		<li className="nav-item">
-			<Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
-				Home
-			</Link>
-		</li>
-		<li className="nav-item">
-			<Link
-				to="/Login"
-				className={window.location.pathname === "/Login" ? "nav-link active" : "nav-link"}
-			>
-			Private Page
-			</Link>
-		</li>
+class Nav extends React.Component {
+	render() {
+		return (
+			<ul className="nav nav-tabs">
+			<li className="nav-item">
+				<Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+					Home
+				</Link>
+			</li>
+			<li className="nav-item">
+				<Link
+					to="/Login"
+					className={window.location.pathname === "/Login" ? "nav-link active" : "nav-link"}
+				>
+				Private Page
+				</Link>
+			</li>
 
-	<li className="nav-item">
-	<Link
-		to="/UserPeeks"
-		className={window.location.pathname === "/UserPeeks" ? "nav-link active" : "nav-link"}
-	>
-	User Peeks
-	</Link>
-</li>
-		</ul>
-  );
+		<li className="nav-item">
+		<Link
+			to="/UserPeeks"
+			className={window.location.pathname === "/UserPeeks" ? "nav-link active" : "nav-link"}
+		>
+		User Peeks
+		</Link>
+	</li>
+			</ul>
+		);
+	}
 }
 
 export default Nav;
