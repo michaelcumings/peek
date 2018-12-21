ed896245329bc9cfbc500dd54118240471a0b194
Nick Sedillos <nick.sedillos@gmail.com>
2018-12-20 20:46:08 -0600

    userNameDisplay styles

--- src/pages/PrivatePage.js
+++ src/pages/PrivatePage.js
@@ -1,39 +1,35 @@
+// Dependencies
 import React, { Component } from "react";
-
 import User from "../components/User.js";
 import Peek from "../components/Peek.js";
 import firebase from "firebase";
-
 
 class PrivatePage extends Component {
   //simple button with on Click
   render() {
     const { userID } = this.props;
     return (
-    <div className="container">
+      <div className="container">
         <div className="row">
-            <div className="col-lg-3"> 
-                <div className="row">
-               <p>{firebase.auth().currentUser.displayName}  <img class = "profilePic"src={firebase.auth().currentUser.photoURL} alt="User profile">
-               </img></p>
-                </div>
-                <div className ="row">              
-                 
-                </div>
+          <div className="col-lg-3"> 
+            <div className="row">
+              <p className="userNameDisplay">
+                {firebase.auth().currentUser.displayName}
+                <img class = "profilePic"src={firebase.auth().currentUser.photoURL} alt="User profile"></img>
+              </p>
             </div>
-            <div className="col-lg-6"> 
-              <User userID={userID} />
-            </div>
-           
-            <div className="col-lg-3 text-right">
+          </div>
+          <div className="col-lg-6"> 
+            <User userID={userID} />
+          </div> 
+          <div className="col-lg-3 text-right">
             <button className =" btn Light"onClick={() => firebase.auth().signOut()}> Logout Out</button>
-            </div>
+          </div>
         </div>            
-          <Peek userID={userID} />
+        <Peek userID={userID} />
       </div>
-
-
     );
   }
 }
+
 export default PrivatePage;
