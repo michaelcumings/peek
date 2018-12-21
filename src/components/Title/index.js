340c326303c7511ae6a887f6275f52d464717e86
Nick Sedillos <nick.sedillos@gmail.com>
2018-12-20 19:18:45 -0600

    Fixed typo in Title.

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -8,13 +8,15 @@ class Title extends Component {
 
     return (
       <div className="container title"> 
-        <h1 className="text-center display-4">
-         PEEK!</h1>
-        <p className="lead">This app allows you to share information about anything and everything. All you have to do is enable locations settings, add images , tags and descriptions.</p>
-        <a className="btn btn-primary" href="https://github.com/nicksedillos/peek" role="button">Learn more @ GitHub</a>
-        
+        <h1 className="text-center display-4">PEEK!</h1>
+        <p className="lead">
+          This app allows you to share information about anything and everything. All you have to do is enable location settings, add images, tags and descriptions.
+        </p>
+        <a className="btn btn-primary" href="https://github.com/nicksedillos/peek" role="button">
+          Learn more @ GitHub
+        </a>
         <hr className="my-4"/>     
-        </div>
+      </div>
     );
   }
 }
